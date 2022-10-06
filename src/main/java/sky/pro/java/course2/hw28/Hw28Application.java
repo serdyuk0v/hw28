023367722d85cc61175c415ae3b1ed802d64c9fa
Serdyukov <and.serdykov@gmail.com>
2022-10-06 08:05:27 +0300

    Empty Homework

---
+++ src/main/java/sky/pro/java/course2/hw28/Hw28Application.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.hw28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw28Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/java/course2/hw28/Hw28ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.hw28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
